2ffd433693cd3db456032f8cd8c340f801d5144a
jbijlsma <jeroen_bijlsma@yahoo.com>
2021-12-23 11:22:44 +0100

    Add prime sakai react files

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,26 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
-
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+//import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop';
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
+    <HashRouter>
+        <ScrollToTop>
+            <App></App>
+        </ScrollToTop>
+    </HashRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Learn more about service workers: http://bit.ly/CRA-PWA
+//serviceWorker.unregister();
